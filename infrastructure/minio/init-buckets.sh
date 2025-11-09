b9299756a002b2963d2ee3dc280a643f944891d3
iizhukov <iizhukov@icloud.com>
2025-11-09 16:48:30 +0300

    ADD: reverse proxy

--- infrastructure/minio/init-buckets.sh
+++ infrastructure/minio/init-buckets.sh
@@ -9,10 +9,12 @@ export MC_HOST_minio=http://minioadmin:minioadmin@minio:9000
 echo "Creating buckets..."
 
 mc mb minio/public --ignore-existing || echo "Bucket 'public' already exists"
+mc mb minio/tracks --ignore-existing || echo "Bucket 'tracks' already exists"
 
 echo "Setting bucket policies..."
 
 mc anonymous set public minio/public || echo "Failed to set public policy for 'public' bucket"
+mc anonymous set public minio/tracks || echo "Failed to set public policy for 'tracks' bucket"
 
 echo "Created buckets:"
 mc ls minio
